e436bebe43bd0e7109f107921d2518f5f57b24d5
telepath01 <47895030+telepath01@users.noreply.github.com>
2022-12-31 11:24:28 -0500

    game Function complete
    Completed
    -Game Function
    -Re-adjusted the game board size
    -Deleted the XL button (to many cells)
    
    Need to complete
    -Add the instructions for the game

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -1,57 +1,79 @@
 "use strict";
 // html calls
+// .
+// .
+// .
 const grid = document.querySelector(".board");
+const gridStyle = document.getElementById("board-style");
 const btn16x16 = document.querySelector(".btn-small");
 let cell = "";
 const buttons = document.querySelectorAll("button");
 const btnSmall = document.querySelector(".btn-small");
+
 // variable sets
+// .
+// .
+// .
 let btnActivated = false;
 const divArr = [];
-const small = 16 * 2;
-const medium = 32 * 2;
-const large = 64 * 2;
-const extraLarge = 128 * 2;
+const small = 16 * 16;
+const medium = 32 * 32;
+const large = 64 * 64;
 
+// functions
+// .
+// .
+// .
+// .
 function populate(num) {
   for (let i = 0; i < num; i++) {
     grid.appendChild(document.createElement("div" + i));
+    document.querySelector("div" + i).classList.add("cell");
+    btnActivated = true;
   }
+  cell = document.querySelectorAll(".cell");
+  Array.from(cell).forEach(function (cell) {
+    cell.addEventListener("mouseover", function () {
+      console.log("works");
+      this.classList.add("block");
+    });
+  });
 }
 
-// btn16x16.addEventListener("click", function () {
-//   if (!btnActivated) {
-//     populate(256);
-//     btnActivated = true;
-//     for (let i = 0; i < 256; i++) {
-//       document.querySelector("div" + i).classList.add("cell");
-//     }
-//     cell = document.querySelectorAll(".cell");
-//     Array.from(cell).forEach(function (cell) {
-//       cell.addEventListener("mouseover", function () {
-//         console.log("works");
-//         this.classList.add("block");
-//       });
-//     });
-//   } else return;
-// });
-
+function gridLayout(num) {
+  gridStyle.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
+  gridStyle.style.gridTemplateRows = `repeat(auto-fill, 1fr)`;
+}
+function resetGame() {
+  gridStyle.style.gridTemplateColumns = `repeat(0, 0)`;
+  gridStyle.style.gridTemplateRows = `repeat(auto-fill, 1fr)`;
+  btnActivated = false;
+  cell.forEach((el) => el.remove());
+  cell = "";
+}
+// event listeners
+// .
+// .
+// .
 buttons.forEach((button) => {
   button.addEventListener("click", function () {
     if (button.className === "reset-btn") {
-      console.log("reset");
+      resetGame();
     }
 
     if (!btnActivated) {
       if (button.className === "btn-small") {
         console.log("small");
-        btnActivated = true;
+        gridLayout(16);
+        populate(small);
       } else if (button.className === "btn-medium") {
         console.log("medium");
+        gridLayout(32);
+        populate(medium);
       } else if (button.className === "btn-large") {
         console.log("large");
-      } else if (button.className === "btn-xl") {
-        console.log("XL");
+        gridLayout(64);
+        populate(large);
       }
     } else return;
   });
